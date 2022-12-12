ac00e57b6c52b86bdccd34a9ced4ae453ae93a7d
raul de serranos <rdeserranos@iuvity.net>
2022-12-12 09:35:43 -0300

    se agrego archivo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({name= "pikachu"}){
+  const [pokemonInfo, setPokemonInfo] = useState(null)
+
+  useEffect(
+    () => {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then(res => res.json())
+      .then(pokemon => {
+        console.log(pokemon)
+        setPokemonInfo(pokemon)
+      });
+     
+    },[name]
+  )
+  return(
+    pokemonInfo && (
+      <span>
+        La pokeId es {pokemonInfo.id} y su nombre es {pokemonInfo.name}
+      </span>
+    )
+  )
 }
-
 export default App;
